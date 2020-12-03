a07effb3f58501231bbe0123f5a53e5702bffb3b
zengqingwang <906088182@qq.com>
2020-12-03 19:49:41 +0800

    fix ManagerBase

--- src/framework/manager/ManagerBase.h
+++ src/framework/manager/ManagerBase.h
@@ -2,13 +2,14 @@
  * @Author: wimidev
  * @Date: 2020-10-19 16:00:05
  * @LastEditors: wimidev
- * @LastEditTime: 2020-12-02 10:53:36
+ * @LastEditTime: 2020-12-03 19:45:55
  * @Description: 管理器基类
  */
 #ifndef MANAGERBASE_H_
 #define MANAGERBASE_H_
+#include "../interface/IRuntime.h"
 
-class ManagerBase{
+class ManagerBase : public IRuntime{
 public:
     virtual ~ManagerBase(){};
     virtual void init()=0;
